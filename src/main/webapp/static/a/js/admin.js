de6501d845fcdc0df9d5a08833e023dfaada1d00
ibetter825 <y825y@qq.com>
2016-12-05 17:21:54 +0800

    vue_datagrid

--- src/main/webapp/static/a/js/admin.js
+++ src/main/webapp/static/a/js/admin.js
@@ -4,27 +4,127 @@
  */
 !(function(){
 	"use strict";
+	/**
+     * 工具
+     */
+    window.tool = {
+    	/**
+    	 * 获取尺寸
+    	 * @returns
+    	 */
+    	dimension: function(tar){
+    		let h = 0, w = 0;
+	        if(tar){
+	        	if(tar.indexOf('.') == 0){//类
+	        		
+	        	}else if(tar.indexOf('#') == 0){//id
+	        		
+	        	}
+	        }else{
+	        	h = document.documentElement.clientHeight || document.body.clientHeight;
+		        w = document.documentElement.clientWidth || document.body.clientWidth;
+	        }
+	        return {width: w, height: h};
+    	},
+        /**
+         * 合并对象，将子对象合并到父对象中
+         * f: 默认父对象
+         * c: 子对象
+         */
+        merge: function(f, c){
+            for(let p in c){
+                if(f[p] != undefined)
+                    f[p] = c[p];
+            }
+        },
+        /**
+         * 复制并合并一个对象
+         * o: 需要复制的对象
+         * c: 合并的子对象
+         */
+        copyAndMerge: function(o, c){
+            if(c == undefined) c = {};
+            let n = {};
+            for(let k in o){
+                if(c[k] != undefined){
+                    n[k] = c[k];
+                    delete c[k];
+                }else
+                    n[k] = o[k];
+            }
+            for(let k in c){
+                if(n[k] == undefined)
+                    n[k] = c[k];
+            }
+            return n;
+        },
+        /**
+         * 复制一个对象
+         */
+        copy: function(o){
+            let n = {};
+            for(let k in o)
+                n[k] = o[k];
+            return n;
+        },
+        /**
+         * 向对象中添加元素
+         * o: 需要添加元素的对象
+         * c: 被添加的元素对象
+         */
+        put: function(o, c){
+            if(o == undefined) o ={};
+            if(c == undefined) c = {};
+            for(let k in c)
+                o[k] = c[k];
+            return o;
+        },
+        /**
+         * 添加节点
+         * f: 父节点
+         * c: classname
+         */
+        create: function(f, c){
+                let d = document.createElement('div');
+                    d.className = c;
+                    f.appendChild(d);
+        }
+    };
+    
 	 window.admin = {};
 	 
-	 
+	 /**
+	  * 默认表格等配置
+	  */
+	 admin.datagrid = {
+		 url: null, //后台请求连接
+		 data: {
+			 total: 0,
+			 rows: [],
+			 footer: null
+		 }, // 数据
+		 tools: [], //弹出层的类型
+   		 columns: [], //计数器 默认0
+         pager: {}, //分页栏
+         onClickRow: null, //datagrid点击行时的事件, 默认null  参数 index,row,event
+         onClickCell: null //单击列
+	 }
 	 /**
      * admin datagrid store
      */
     const s_datagrid = {
         state: {
-            tools: [], //弹出层的类型
-            columns: [], //计数器 默认0
-            pager: {}
+            options: admin.datagrid
         },
         mutations: {
-            a_tools(state, tools) {
-                state.tools = tools;
-            },
-            a_columns(state, columns) {
-                state.columns = columns;
-            },
-            a_pager(state, pager) {//获取当前指定的视图
-                state.pager = pager;
+        	a_options(state, options) {
+                //直接传options对象
+                if(options instanceof Array){
+                    let opt = options[0];
+                    for(let k in opt)
+                        state.options[k] = opt[k];
+                }else
+                    state.options = options;
             }
         }
     };
@@ -45,15 +145,15 @@
         props: ['props'],
         template: `<div v-if="tools.length > 0" class="tools">
 			            <ul class="toolbar">
-			                <li v-for="tool in tools"><span><img :src="tool.icon" /></span>{{ tool.title }}</li>
+			                <li v-for="tool in options.tools"><span><img :src="tool.icon" /></span>{{ tool.title }}</li>
 			            </ul>
 			        </div>`,
         data: function(){
             return {};
         },
         computed: {
-        	tools(){
-        		return this.$store.state.datagrid.tools;
+        	options(){
+        		return this.$store.state.datagrid.options;
         	}
         },
         watch: {
@@ -69,35 +169,142 @@
         template: `<table class="tablelist">
 			            <thead>
 		                <tr>
-		                    <th v-for="col in columns">
-		                    	<input v-if="col.checkbox" name="" type="checkbox" value=""/>
+		                    <th v-for="col in options.columns" :style="style(col, 'th')">
+		                    	<input v-if="col.checkbox" name="checkedAll" @click.stop="checkAll()" type="checkbox" value="" v-model="checkedAll"/>
 		                    	{{ col.title }}	<i v-if="col.sortable" class="sort"><img src="/static/a/images/px.gif" /></i>
 		                    </th>
 		                </tr>
 		            </thead>
 		            <tbody>
-		                <tr>
-		                    <td><input name="" type="checkbox" value="" /></td>
-		                    <td>20130908</td>
-		                    <td>王金平幕僚：马英九声明字字见血 人活着没意思</td>
-		                    <td>admin</td>
-		                    <td>江苏南京</td>
-		                    <td>2013-09-09 15:05</td>
-		                    <td>已审核</td>
-		                    <td><a href="#" class="tablelink">查看</a> <a href="#" class="tablelink"> 删除</a></td>
+		                <tr v-for="(row, ri) in options.data.rows" @click.stop="clickRow(ri, row, $event)" :class="[ri % 2 == 0 ? 'odd' : '', selecteds[ri]]">
+		                	<td v-for="(col, ci) in options.columns" :style="style(col, 'td')">
+		                		<span v-if="col.checkbox"><input :name="col.field" type="checkbox" :value="row[col.field]" v-model="checkeds" /></span>
+		                		<span v-else @click.stop="clickCell(ri, col.field, row[col.field], $event, row)" v-html="formatter(row[col.field], row, ri, ci)"></span>
+		                	</td>
 		                </tr>
 		            </tbody>
 		        </table>`,
         data: function(){
-            return {};
+            return {
+            	checkedAll: false,
+            	checkeds:　[],
+            	selecteds: []
+            };
         },
         computed: {
-        	columns(){
-        		return this.$store.state.datagrid.columns;
+        	options(){
+        		return this.$store.state.datagrid.options;
+        	},
+        	client(){
+        		return tool.dimension();//获取当前窗口的宽高
+        	}
+        },
+        methods: {
+        	/**
+        	 * 格式化数据, 默认直接返回值
+        	 */
+        	formatter: function(value, row, ri, ci){
+        		let fmt = this.options.columns[ci]['formatter'];
+        		if(fmt)
+        			return fmt(value, row, ri);
+        		return value;
+        	},
+        	/**
+        	 * 样式
+        	 */
+        	style: function(column, tp){
+        		let style = {};
+        		if(tp === 'th') {
+        			if(column.halign)
+            			style.textAlign = column.halign;
+        			else if(column.align)
+            			style.textAlign = column.align;
+        		} else {
+        			if(column.align)
+            			style.textAlign = column.align;
+        		}
+        		
+        		let width = column.width;
+        		if(width != undefined){
+        			if(typeof width == 'number')
+        				style.width = width + 'px';
+        			else
+        				style.width = width;
+        		}else
+        			style.width = '100px';
+        		return style;
+        	},
+        	/**
+        	 * th与td的宽度
+        	 */
+        	width: function(column){
+        		return column.width == undefined ? '' : column.width;
+        	},
+        	/**
+        	 * 全选
+        	 */
+        	checkAll:　function(){
+        		let checkedAll = this.checkedAll;
+        		if(checkedAll){
+        			this.checkedAll = false;
+        			this.checkeds = []; 
+        			this.selecteds = [];
+        		}else{
+        			this.checkedAll = true;
+        			let rows = this.options.data.rows;
+        			this.checkeds = [];
+        			let col = this.options.columns[0];
+        			for(let i in rows){
+        				this.checkeds.push(rows[i][col.field]);
+        				this.selecteds[i] = 'selected';
+        			}
+        		}
+        	},
+        	/**
+        	 * 单击行
+        	 */
+        	clickRow: function(index, row, event){
+        		let clickRow = this.options.onClickRow;
+        		if(clickRow) clickRow(index, row, event);
+        		
+        		//默认点击行事件需要选中复选框
+        		let col = this.options.columns[0];
+        		let cb = col.checkbox;
+        		if(cb){
+        			let size = this.options.data.rows.length;
+        			let pk = row[col.field]; //复选框的字段
+        			let checkeds = this.checkeds;
+            		let i = checkeds.indexOf(pk);
+            		if(i == -1){
+            			checkeds.push(pk);
+            			this.selecteds[index] = 'selected';
+            			if(size <= checkeds.length)
+            				this.checkedAll = true;
+            		} else {
+            			checkeds.splice(i, 1);
+            			this.selecteds[index] = '';
+            			this.checkedAll = false;
+            		}
+        		}
+        	},
+        	/**
+        	 * 单击列
+        	 */
+        	clickCell: function(index, field, value, event, row){
+        		let clickCell = this.options.onClickCell;
+        		if(clickCell) clickCell(index, field, value, event);
+        		//不论有没有列点击事件都需要调用行点击事件
+        		this.clickRow(index, row, event);
         	}
         },
         watch: {
-
+        	
+        },
+        beforeCreate: function(){
+        	console.log('实例创建之前');
+        },
+        created: function(){
+        	
         }
 	};
 	 
@@ -130,7 +337,6 @@
 
         }
 	};
-	
 	 
 	 /**
      * 静态datagrid vm实例
@@ -175,9 +381,10 @@
 	    });
 	}
 	
-	admin.datagrid = function(tools, columns, pager){
+	admin.datagrid = function(tools, columns, data, pager){
 		admin.datagridVue();
-		store.commit('a_tools', tools);
-		store.commit('a_columns', columns);
+		store.commit('a_options', tool.copyAndMerge(admin.datagrid, {tools: tools, columns: columns, data: data, onClickCell: function(index, field, value){
+			layer.msg('当前的值是:'+value);
+		}}));
 	}
 })();
